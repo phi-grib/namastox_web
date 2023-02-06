a66e509b60f0e436e08200c0275cda7443d8e32d
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-02-06 13:19:47 +0100

    navbar first version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { SplitComponent } from 'angular-split';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'namastox_web';
+  constructor() {}
+  navbarleft = 60;
+  navbarright = 40;
+
+  @ViewChild('mySplit')
+  mySplitEl!: SplitComponent;
+  // area size
+  _size1=100;
+  _size2=0;
+  get size1() {
+    return this._size1;
+  }
+  
+  set size1(value) {
+      this._size1 = value;
+  }
+  get size2() {
+    return this._size2;
+  }
+  
+  set size2(value) {
+      this._size2 = value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AngularSplitModule } from 'angular-split';
 import { AppComponent } from './app.component';
+import { NavbarLeftComponent } from './navbar-left/navbar-left.component';
+import { NavbarRightComponent } from './navbar-right/navbar-right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarLeftComponent,
+    NavbarRightComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularSplitModule
   ],
   providers: [],
   bootstrap: [AppComponent]
