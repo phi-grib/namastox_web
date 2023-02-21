b15039edfe80cd956ab1b63480e69dce6f0141cb
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-02-21 12:24:40 +0100

    good working code first version

--- src/app/select-ra/select-ra.component.ts
+++ src/app/select-ra/select-ra.component.ts
@@ -16,47 +16,7 @@ export class SelectRaComponent  {
   }
 
   loadRA(){
-    this.ra.pending_tasks = [];
-    this.results.resultSelected = '';
-    this.results.decisionSelected = '';
-     /**Get step of default RA */
-     this.commonService.getSteps(this.ra.name).subscribe((result:any) => {
-      console.log("STEPS")
-      this.ra.listSteps = [...result];
-    },
-    error=> {
-      console.log(error)
-    })
-    /**Get general info ra */
-    this.commonService.getGeneralInfo(this.ra.name).subscribe(result => {
-      this.ra.general_information = result
-    },error=> {
-      console.log(error)
-    })
-    /**Get status of RA */
-    this.commonService.getStatus(this.ra.name).subscribe(result => {
-      this.ra.status = result.ra
-    }, error => {
-      console.log(error)
-    })
-       /**Get results of RA */
-        this.commonService.getResults(this.ra.name).subscribe(result => {
-         this.ra.results = result
-         this.func.separateResults();
-       }, error => {
-        console.log("error")
-         console.log(error)
-       })
-       /** Get pending tasks */
-       this.commonService.getPendingTasks(this.ra.name).subscribe(result => {
-        this.ra.pending_tasks = result;
-      },error => {
-        console.log("error in pending tasks");
-      })
-      setTimeout(() => {
-        this.global.interfaceVisible = true;
-        this.commonService.AutoGenerateForm();
-      }, 500);
+      this.func.refreshRA();
   }
   
   loadStep(){
@@ -75,21 +35,11 @@ export class SelectRaComponent  {
 
   deleteRA(){
     this.commonService.deleteRA(this.ra.name).subscribe(result => {
-      this.global.interfaceVisible = false;
       this.commonService.getRaList().subscribe((result:any) => {
         this.ra.listRA = [...result];
         this.ra.name = this.ra.listRA[this.ra.listRA.length-1];
-        /**Get general info ra */
-        this.commonService.getGeneralInfo(this.ra.name).subscribe(result => {
-        this.ra.general_information = result
-      },error=> {
-        console.log(error)
-      })
         this.func.refreshRA();
       })
-      setTimeout(() => {
-        this.global.interfaceVisible = true;
-      }, 500);
     },error => {
       console.log(error)
     })
@@ -109,21 +59,13 @@ export class SelectRaComponent  {
 
   newRA(){
      this.commonService.createRA(this.newRAname).subscribe(result=>{
-      this.global.interfaceVisible = false;
       this.commonService.getRaList().subscribe((result:any) => {
         this.ra.listRA = [...result];
         this.ra.name = this.ra.listRA[this.ra.listRA.length-1];
-        /**Get general info ra */
-        this.commonService.getGeneralInfo(this.ra.name).subscribe(result => {
-        this.ra.general_information = result
-      },error=> {
-        console.log(error)
-      })
         this.func.refreshRA();
       })
       setTimeout(() => {
-       $('#closeMenubtn').click();
-        this.global.interfaceVisible = true;
+        document.getElementById('menubtn').click();
       }, 500);
      },error =>{
       console.log("error:")
