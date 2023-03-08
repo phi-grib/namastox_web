98a329bea3ef4126aacd131fc9b137fdeea3e4be
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-03-08 17:28:25 +0100

    first version redirect to task or past task

--- src/app/workflow/workflow.component.ts
+++ src/app/workflow/workflow.component.ts
@@ -2,21 +2,20 @@ import {
   Component,
   ViewChild,
   ElementRef,
-  OnInit,
-  AfterViewInit,
+  OnInit
 } from '@angular/core';
 import mermaid from 'mermaid';
 import { CommonService } from '../common.service';
-import { RA } from '../globals';
+import { PendingTasks, RA, Results } from '../globals';
 @Component({
   selector: 'app-workflow',
   templateUrl: './workflow.component.html',
   styleUrls: ['./workflow.component.scss']
 })
 
-export class WorkflowComponent implements OnInit, AfterViewInit { 
+export class WorkflowComponent implements OnInit { 
 
-  constructor(private ra: RA,private commonService:CommonService){
+  constructor(private ra: RA,private commonService:CommonService,private pendingTasks:PendingTasks,private results:Results){
 
   }
   @ViewChild('mermaidDiv', { static: false }) mermaidDiv: ElementRef;
@@ -34,31 +33,60 @@ export class WorkflowComponent implements OnInit, AfterViewInit {
     );
   }
 
-  redirectToTask(){
-    var task = 'result'
-    if(task == 'result'){
-      $('#pills-results-tab').click();
-      $('#pills-pendingtask-tab').click();
-    }else{
-      $('#pills-decisions-tab').click();
-      $('#pills-pendingdecisions-tab').click();
-    }
-
+  selectPastTask(typeTask,taskName){
+    let tableID = typeTask === 'results' ? '#dtResults' : '#dtDecisions'
+    const table = document.querySelector(tableID);
+    table.querySelectorAll('tr').forEach((row) => {
+      const cells = row.querySelectorAll('td');
+      if(cells[0] != undefined && cells[0].textContent === taskName){
+        row.click();
+      }
+    });
   }
 
-  ngAfterViewInit(): void {
-    // setTimeout(() => {
-    //   if(this.ra.listRA.length > 0 ){
-    //         this.flowchartRefresh();
-    //   }
-    // }, 300);
+  redirectToTask(typeTask,pending,taskname){
+    if(typeTask == 'results'){
+      $('#pills-results-tab').click();
+      if(pending){
+        $('#pills-pendingtask-tab').click();
+        // TO DO
+        // console.log(this.pendingTasks.results)
+        // let selector:any = document.getElementById("selectPendingResult");
+        // const option = selector.options[1]
+        // option.click();
+      }else{
+        $('#pills-pasttasks-tab').click();  
+        this.selectPastTask(typeTask,taskname)
+      }
+    }else{
+      $('#pills-decisions-tab').click();
+      if(pending){
+        $('#pills-pendingdecisions-tab').click();
+      }else{
+        $('#pills-pastdecisions-tab').click();
+        this.selectPastTask(typeTask,taskname)
+      }
+    }
+  }
+
+  checkType(taskName){
+    // RESULTS
+    const pendingTaskResults = this.pendingTasks.results.find(task => task.id == taskName)
+    const pastTaskResults = this.results.results.find(task => task.id == taskName)
+    if(pendingTaskResults) this.redirectToTask('results',true,taskName) 
+    if(pastTaskResults) this.redirectToTask('results',false,taskName)
+    // DECISIONS
+    const pendingTaskDecisions = this.pendingTasks.decisions.find(task => task.id == taskName)
+    const pastTaskDecisions = this.results.decisions.find(task => task.id == taskName)
+    if(pendingTaskDecisions) this.redirectToTask('decisions',true,taskName) 
+    if(pastTaskDecisions) this.redirectToTask('decisions',false,taskName)
+
   }
 
   ngOnInit(): void {
     (window as any).onA = (nodeName) => {
-      if(nodeName === 'A' && this.ra.status.step == 1){
-        this.redirectToTask();
-      }
+      console.log("cuantas veces")
+      this.checkType(nodeName);
     };
 
     mermaid.initialize({
