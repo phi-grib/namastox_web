ac134a887a75183ad222c5c0beff3f1a3372787d
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-03-28 11:24:10 +0200

    align content

--- src/app/decisions/decisions.component.ts
+++ src/app/decisions/decisions.component.ts
@@ -48,7 +48,7 @@ export class DecisionsComponent implements OnInit {
   drawMol(){
     if(this.results.decisionSelected.substance.length > 0 ){
      for (let index = 0; index < this.results.decisionSelected.substance.length; index++) {
-      let smilesDrawer = new SmilesDrawer.Drawer({ width: 100, height: 150 });
+      let smilesDrawer = new SmilesDrawer.Drawer({ width: 200, height: 150 });
       SmilesDrawer.parse(this.results.decisionSelected.substance[index].SMILES, function (tree) {
         smilesDrawer.draw(tree, 'decisionCanvas'+index, 'light', false);
     },  function (err) {
