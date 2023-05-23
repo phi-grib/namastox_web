76e178bebce4f024237a27a22df6601cee752c4b
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-05-23 13:39:55 +0200

    static html in tasks done

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from '../common.service';
 import { PendingTasks, RA, Results } from '../globals';
-import { FormGroup } from '@angular/forms';
+
+import { FormGroup, FormControl } from '@angular/forms';
+
 import { FormlyFieldConfig } from '@ngx-formly/core';
 import { CommonFunctions } from '../common.functions';
 import { UpdateService } from '../update.service';
@@ -14,6 +16,9 @@ import * as SmilesDrawer from 'smiles-drawer';
   styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent implements OnInit {
+  
+
+
   loadForm: boolean = false;
   form = new FormGroup({});
   model: any;
@@ -22,6 +27,12 @@ export class TasksComponent implements OnInit {
   pending_task: any;
   link: Blob;
   objectKeys = Object.keys;
+  parameter: string;
+  value: string;
+  unit: string;
+  uncertainty: string;
+  result_link: File;
+  summary: string;
   constructor(public ra: RA, private commonService: CommonService, public pendingTasks: PendingTasks, private func: CommonFunctions, public results: Results, private updateService: UpdateService, private toastr: ToastrService) {
   }
 
@@ -38,7 +49,6 @@ export class TasksComponent implements OnInit {
       }
     })
   }
-
   downloadFile() {
     // create object Blob
     const blob = new Blob([this.link], { type: 'application/octet-stream' });
@@ -81,8 +91,10 @@ setTimeout(() => {
     const EXPERIMENT = ['idem','value','unit','uncertainty','result_link','summary'];
     const REPORT = ['report','result_link','summary'];
     this.form = new FormGroup({});
+    
     this.commonService.getPendingTask(this.ra.name, this.pending_task_selected).subscribe({
       next: (result) => {
+       
         let template_keys = []
         this.pending_task = result;
         this.model = this.pending_task.result;
@@ -118,35 +130,42 @@ setTimeout(() => {
 
 
   }
-  onSubmit(model: any) {
+  onSubmit() {
     this.loadForm = false;
-    if (model['result_link']){
-      this.sendlink(model['result_link'])
-      model['result_link'] = model['result_link'][0].name;
-    }
-    this.updateService.updateResult(this.ra.name, model).subscribe({
-      next: (result) => {
-        if (result['success']) {
-          this.pendingTasks.results = [];
-          this.func.refreshRA();
-          setTimeout(() => {
-            if (this.pendingTasks.results.length) {
-              this.pending_task_selected = this.pendingTasks.results[0].id;
-              this.createform();
-            }
-          }, 1000);
-          this.toastr.success('RA ' + this.ra.name, 'SUCCESSFULLY UPDATED', {
-            timeOut: 5000, positionClass: 'toast-top-right'
-          });
-        }
-      },
-      error: (e) => {
-        this.toastr.error('Check the console to see more information', 'Unexpected Error', {
-          timeOut: 5000, positionClass: 'toast-top-right'
-        });
-        console.error(e)
-      },
-    });
+    this.model['value'] = this.value;
+    this.model['summary'] = this.summary;
+    this.model['result_link'] = this.result_link;
+    this.model['unit'] = this.unit;
+    this.model['uncertainty'] = this.uncertainty;
+
+     if(this.result_link){
+       this.sendlink(this.result_link);
+       this.model['result_link'] = this.result_link[0].name;
+     }
+
+     this.updateService.updateResult(this.ra.name, this.model).subscribe({
+       next: (result) => {
+         if (result['success']) {
+           this.pendingTasks.results = [];
+           this.func.refreshRA();
+           setTimeout(() => {
+             if (this.pendingTasks.results.length) {
+               this.pending_task_selected = this.pendingTasks.results[0].id;
+               this.createform();
+             }
+           }, 1000);
+           this.toastr.success('RA ' + this.ra.name, 'SUCCESSFULLY UPDATED', {
+             timeOut: 5000, positionClass: 'toast-top-right'
+           });
+         }
+       },
+       error: (e) => {
+         this.toastr.error('Check the console to see more information', 'Unexpected Error', {
+           timeOut: 5000, positionClass: 'toast-top-right'
+         });
+         console.error(e)
+       },
+     });
   }
 
 
