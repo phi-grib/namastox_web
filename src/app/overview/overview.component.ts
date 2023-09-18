cdd9489dd41206f58dc5619bc12addf3b62f8daf
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-09-18 10:10:10 +0200

    clean code overview comp

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -3,45 +3,49 @@ import { RA, Results } from '../globals';
 import * as SmilesDrawer from 'smiles-drawer';
 import { CommonService } from '../common.service';
 
-
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.scss']
+  styleUrls: ['./overview.component.scss'],
 })
-
-export class OverviewComponent implements OnInit{
+export class OverviewComponent implements OnInit {
   objectKeys = Object.keys;
-  @ViewChild('OverviewCanvas', { static: true }) OverviewCanvas: ElementRef<HTMLCanvasElement>;
+  @ViewChild('OverviewCanvas', { static: true })
+  OverviewCanvas: ElementRef<HTMLCanvasElement>;
 
-  constructor(public ra: RA,private commonService: CommonService,public results: Results){
-    
-
-  }
+  constructor(
+    public ra: RA,
+    private commonService: CommonService,
+    public results: Results
+  ) {}
   ngOnInit(): void {
-     this.commonService.updateOverviewCanvas$.subscribe( status => {
-       setTimeout(() => {
-       if(!status) this.drawMol();
-       }, 300);
-     })
+    this.commonService.updateOverviewCanvas$.subscribe((status) => {
+      setTimeout(() => {
+        if (!status) this.drawMol();
+      }, 300);
+    });
   }
-   drawMol(){
-      var smile = '';
-      //check if user insert a valid substance
-      const substances = this.ra.general_information?.general?.substances; 
-      smile = substances?.length > 0 ? substances[0].smiles : null;
-      
-     if(smile){
-       let smilesDrawer = new SmilesDrawer.Drawer({ width: 200, height: 150 });
-       SmilesDrawer.parse(smile, function (tree) {
-         smilesDrawer.draw(tree, 'substanceCanvas', 'light', false);
-     },  function (err) {
-       console.log(err);
-     });
-     }else{
+  drawMol() {
+    var smile = '';
+    //check if user insert a valid substance
+    const substances = this.ra.general_information?.general?.substances;
+    smile = substances?.length > 0 ? substances[0].smiles : null;
+
+    if (smile) {
+      let smilesDrawer = new SmilesDrawer.Drawer({ width: 200, height: 150 });
+      SmilesDrawer.parse(
+        smile,
+        function (tree) {
+          smilesDrawer.draw(tree, 'substanceCanvas', 'light', false);
+        },
+        function (err) {
+          console.log(err);
+        }
+      );
+    } else {
       const canvas = this.OverviewCanvas.nativeElement;
       const context = canvas.getContext('2d');
       context.clearRect(0, 0, canvas.width, canvas.height);
-     }
- } 
+    }
+  }
 }
