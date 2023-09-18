c032178bb7f452507db5095755335d3820ba2a01
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-09-18 10:08:33 +0200

    clean code model documentation comp

--- src/app/model-documentation/model-documentation.component.ts
+++ src/app/model-documentation/model-documentation.component.ts
@@ -3,24 +3,64 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'model-documentation',
   templateUrl: './model-documentation.component.html',
-  styleUrls: ['./model-documentation.component.scss']
+  styleUrls: ['./model-documentation.component.scss'],
 })
-export class ModelDocumentationComponent  {
+export class ModelDocumentationComponent {
   @Input() documentation: any;
   objectKeys = Object.keys;
 
- genInformationKeys = ['ID', 'Version', 'Model_title', 'Model_description', 'Keywords', 'Contact', 'Institution', 'Date', 'Endpoint','Endpoint_units', 'Interpretation', 'Dependent_variable', 'Species','Limits_applicability', 'Experimental_protocol', 'Model_availability','Data_info']
+  genInformationKeys = [
+    'ID',
+    'Version',
+    'Model_title',
+    'Model_description',
+    'Keywords',
+    'Contact',
+    'Institution',
+    'Date',
+    'Endpoint',
+    'Endpoint_units',
+    'Interpretation',
+    'Dependent_variable',
+    'Species',
+    'Limits_applicability',
+    'Experimental_protocol',
+    'Model_availability',
+    'Data_info',
+  ];
 
- algorithmKeys =  ['Algorithm', 'Software', 'Descriptors', 'Algorithm_settings','AD_method', 'AD_parameters', 'Goodness_of_fit_statistics','Internal_validation_1', 'Internal_validation_2', 'External_validation','Comments']
+  algorithmKeys = [
+    'Algorithm',
+    'Software',
+    'Descriptors',
+    'Algorithm_settings',
+    'AD_method',
+    'AD_parameters',
+    'Goodness_of_fit_statistics',
+    'Internal_validation_1',
+    'Internal_validation_2',
+    'External_validation',
+    'Comments',
+  ];
 
- otherInformationKeys = ['Other_related_models', 'Date_of_QMRF', 'Date_of_QMRF_updates','QMRF_updates', 'References', 'QMRF_same_models', 'Mechanistic_basis','Mechanistic_references', 'Supporting_information', 'Comment_on_the_endpoint','Endpoint_data_quality_and_variability', 'Descriptor_selection']
+  otherInformationKeys = [
+    'Other_related_models',
+    'Date_of_QMRF',
+    'Date_of_QMRF_updates',
+    'QMRF_updates',
+    'References',
+    'QMRF_same_models',
+    'Mechanistic_basis',
+    'Mechanistic_references',
+    'Supporting_information',
+    'Comment_on_the_endpoint',
+    'Endpoint_data_quality_and_variability',
+    'Descriptor_selection',
+  ];
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  isObject(value): boolean {
+    return typeof value === 'object';
   }
-
-  isObject(value): boolean{
-    return typeof value === 'object'; 
-      }
-
-
 }
