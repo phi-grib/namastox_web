e569a928cd71e510467145673c27c90d2e62d57a
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-12-20 17:57:25 +0100

    starting work in the notes interface

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class NotesComponent {
 
+  listOfNotes =  [
+    {
+      id: 'd1223',
+      title: 'title1',
+      date: '21-02-2020'
+
+    },
+    {
+      id: '23123',
+      title: 'title2',
+      date: '21-02-2030'
+
+    },
+    {
+      id: 'd1223',
+      title: 'title3',
+      date: '11-02-2021'
+
+    }
+  ]
 }
