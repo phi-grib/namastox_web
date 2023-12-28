d8b50dc6123a1eaf4b7445cf04aaf2c9f456f3fc
AdrianCabreraPhi <adriancabproven@gmail.com>
2023-12-28 12:03:11 +0100

    note form

--- src/app/note-form/note-form.component.ts
+++ src/app/note-form/note-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DateObject } from 'ngx-bootstrap/chronos/types';
 import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
@@ -9,6 +10,13 @@ import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 export class NoteFormComponent implements OnInit {
 
   datePickerConfig: Partial<BsDatepickerConfig>;
+
+  // variables for test (change when receive object from backend).
+  title:string = "";
+  id: string = '';
+  information: string = "";
+  date:string; // fix error: Format incorrect
+  
   constructor() {
     this.datePickerConfig = Object.assign({}, {
       containerClass: 'theme-default',
@@ -18,9 +26,24 @@ export class NoteFormComponent implements OnInit {
   ngOnInit(): void {
   
   }
+  /**
+   * improve this function, change for native form function
+   */
+  resetFields(){
+    this.title = ""
+    this.id =""
+    this.information = ""
+    this.date = "";
+  }
 
   onSubmit(){
-    
+    console.log("title",this.title)
+    console.log("ID",this.id)
+    console.log("date",this.date)
+    console.log("information",this.information)
+    this.resetFields();
+    //insert function to add note  
+    // if it's done , call function: list notes again
   }
 
 }
