f812ea81d270de7625cf3f8eb35ed8c264ef3b04
AdrianCabreraPhi <adriancabproven@gmail.com>
2024-03-04 13:54:38 +0100

    clean code

--- src/app/autolink-pipe.pipe.ts
+++ src/app/autolink-pipe.pipe.ts
@@ -9,12 +9,11 @@ export class AutolinkPipePipe implements PipeTransform {
 
   transform(value: string): SafeHtml {
     const urlRegex = /(https?:\/\/[^\s]+)/g;
-     // Reemplaza los enlaces con la etiqueta de anclaje HTML
+    
      const result = value?.replace(urlRegex, (url) => {
       return `<a href="${url}" target="_blank">${url}</a>`;
     });
 
-   // Sanitiza el resultado para permitir HTML
    return this.sanitizer.bypassSecurityTrustHtml(result);
   }
 
