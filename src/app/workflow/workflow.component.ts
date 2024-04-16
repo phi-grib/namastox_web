3fd60545edb1f4ac570005ba19b8533ecb4efde3
AdrianCabreraPhi <adriancabproven@gmail.com>
2024-04-16 12:02:55 +0200

    button download workflow

--- src/app/workflow/workflow.component.ts
+++ src/app/workflow/workflow.component.ts
@@ -2,6 +2,8 @@ import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import mermaid from 'mermaid';
 import { CommonService } from '../common.service';
 import { PendingTasks, RA, Results } from '../globals';
+import html2canvas from 'html2canvas';
+
 @Component({
   selector: 'app-workflow',
   templateUrl: './workflow.component.html',
@@ -15,7 +17,9 @@ export class WorkflowComponent implements OnInit {
     private results: Results
   ) {}
   @ViewChild('mermaidDiv', { static: false }) mermaidDiv: ElementRef;
+  @ViewChild('graphDiv', { static: false }) graphDiv: ElementRef;
 
+    
   flowchartRefresh() {
     const element: any = this.mermaidDiv.nativeElement;
     mermaid.render('graphDiv', this.ra.workflow, (svgCode, bindFunctions) => {
@@ -114,7 +118,6 @@ export class WorkflowComponent implements OnInit {
     (window as any).onA = (nodeName) => {
       this.checkType(nodeName);
     };
-
     mermaid.initialize({
       securityLevel: 'loose',
       flowchart: {
@@ -123,11 +126,51 @@ export class WorkflowComponent implements OnInit {
       //  curve: 'stepAfter',
       },
     });
-
     mermaid.init();
-
     this.commonService.refreshWorklfow$.subscribe(() => {
       this.flowchartRefresh();
     });
   }
-}
+  /**
+   * Image format export
+   */
+  exportWorkflow(){ 
+    this.guardarImagen();
+    }
+    guardarImagen() {
+      // Obtiene el elemento SVG
+      const svgElement = document.getElementById('graphDiv');
+    
+      // Crea un canvas
+      const canvas = document.createElement('canvas');
+      const context = canvas.getContext('2d');
+
+      // Obtiene las dimensiones del SVG
+      const svgBounds = svgElement.getBoundingClientRect();
+      // Establece el tamaño del canvas para que coincida con las dimensiones del SVG
+    canvas.width = svgBounds.width;
+    canvas.height = svgBounds.height;
+
+      // Crea una nueva imagen SVG
+      const xml = new XMLSerializer().serializeToString(svgElement);
+      const svg64 = btoa(xml);
+      const image64 = 'data:image/svg+xml;base64,' + svg64;
+    
+      // Carga la imagen SVG en un objeto de imagen
+      const img = new Image();
+      img.onload = () => {
+        // Dibuja la imagen SVG en el canvas
+        context.drawImage(img, 0, 0);
+    
+        // Convierte el canvas en una URL de datos de imagen
+        const imgData = canvas.toDataURL('image/png');
+    
+        // Crea un enlace temporal para descargar la imagen
+        const link = document.createElement('a');
+        link.setAttribute('download', 'screenshot.png');
+        link.setAttribute('href', imgData);
+        link.click();
+      };
+      img.src = image64;
+    }
+  };
