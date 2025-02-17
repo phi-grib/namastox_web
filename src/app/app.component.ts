5d8e29788e71db366c64111f1bae5afbe275e64e
AdrianCabreraPhi <adriancabproven@gmail.com>
2025-02-17 12:54:19 +0100

    new desing user information - logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,14 +21,18 @@ export class AppComponent implements OnInit {
   ) {}
   ngOnInit(): void {
     console.log("keycloak function")
-      this.keycloackService.getSessionUser().subscribe({
+      // this.keycloackService.getSessionUser().subscribe({
      
-        next: (result:any) => {
-          console.log("function get username")
-          console.log("result:",result['username'])
-          this.user.username = result['username']
-        }
-      })
+      //   next: (result:any) => {
+      //     console.log("function get username")
+      //     console.log("result:",result['username'])
+      //     this.user.username = result['username']
+      //   }
+      // })
+      
+      // DEVELOPMENT
+      this.user.username = "test123"
+
 
     this.commonService.getRaList().subscribe({
       next: (result: any) => {
