7a69d85918535fce182b074e262bc66eb0970d4d
AdrianCabreraPhi <adriancabproven@gmail.com>
2026-01-13 16:42:46 +0100

    improved code : migrate code to angular v17 syntax

--- src/app/notes/notes.component.ts
+++ src/app/notes/notes.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RA, User } from '../globals';
-import { CommonService } from '../common.service';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-notes',
@@ -9,47 +7,7 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./notes.component.scss']
 })
 export class NotesComponent{
-  constructor(private toastr: ToastrService,public ra:RA,private commonService:CommonService,  public user: User){
+  constructor(public ra:RA, public user: User){
 
-  }
-  note = {};
-
-  selectNote(id:string){
-    this.ra.note = this.ra.notes.find(note  => note.id === id)
-  }
-
-  deleteNote(note){
-
-    this.commonService.deleteNote(this.ra.name,note.id).subscribe({
-      next: (result)=> {
-        this.toastr.success(
-          'Note ' + note.title,
-          'SUCCESSFULLY DELETED',
-          {
-            timeOut: 5000,
-            positionClass: 'toast-top-right',
-          }
-        );
-        this.ra.note = {}
-        this.commonService.getNotes(this.ra.name).subscribe({
-          next: (result)=> {
-            this.ra.notes = result
-            this.commonService.getStatus(this.ra.name).subscribe({
-              next:(result) => {
-                this.ra.status = result['ra']
-              },
-              error: (e) => {
-                console.log(e)
-              }
-            })
-          },
-          error: (e)=>{
-            console.log(e);
-          }
-        })
-      }, error: (e)=> {
-        console.log(e);
-      }
-    })
   }
 }
