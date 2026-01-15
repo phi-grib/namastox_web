d0d49b72055a79fc0173771e4665c4e7be0ec062
AdrianCabreraPhi <adriancabproven@gmail.com>
2026-01-15 15:35:23 +0100

    improved code: new component nav-tasks

---
+++ src/app/nav-tasks/nav-tasks.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-tasks',
+  templateUrl: './nav-tasks.component.html',
+  styleUrl: './nav-tasks.component.scss'
+})
+export class NavTasksComponent {
+
+}
