7f36b0c0a35773fac1a9b93f0f12f99df6784501
AdrianCabreraPhi <adriancabproven@gmail.com>
2026-02-12 11:18:37 +0100

    improved load spinner code

--- src/app/spinner/spinner.component.ts
+++ src/app/spinner/spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './spinner.component.scss'
 })
 export class SpinnerComponent {
+  @Input('isLoading') isLoading: boolean;
 
 }
